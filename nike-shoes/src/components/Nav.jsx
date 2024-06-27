83a9400134642d06755fab215ae861ec1d5e44a5
Richard Husey <chrisgogo68@gmail.com>
2024-06-27 10:24:32 -0400

    feat: implement responsive

--- nike-shoes/src/components/Nav.jsx
+++ nike-shoes/src/components/Nav.jsx
@@ -8,20 +8,25 @@ const ROUTES = ["Home", "About", "Services", "Pricing", "Contact"];
 const Nav = () => {
   const [isMobileMenuShown, setIsMobileMenuShown] = useState(false);
   return (
-    <nav className="flex flex-wrap items-center justify-between">
+    <nav className="z-10 relative flex flex-wrap items-center justify-between">
       <a href="#">
         <NikeLogo className="h-20 w-20" />
       </a>
-      <button onClick={()=> setIsMobileMenuShown(!isMobileMenuShown)} className="lg:hidden rounded-lg p-2 hover:bg-gray-100 focus:ring-2 focus:ring-gray-200">
+      <button
+        onClick={() => setIsMobileMenuShown(!isMobileMenuShown)}
+        className="lg:hidden rounded-lg p-2 hover:bg-gray-100 focus:ring-2 focus:ring-gray-200"
+      >
         <RxHamburgerMenu size={25} />
       </button>
       {/* Menu List */}
-      <div className={`${!isMobileMenuShown && `hidden`} w-full lg:w-auto lg:block`}>
+      <div
+        className={`${!isMobileMenuShown && `hidden`} w-full lg:w-auto lg:block`}
+      >
         <ul className="lg:space-x-8 flex flex-col lg:flex-row bg-gray-50 text-lg border border-gray-100 p-4 rounded-lg lg:bg-transparent lg:border-none">
           {ROUTES.map((route, index) => {
             return (
               <li
-                className={`py-2 px-3 cursor-pointer rounded ${index === 0 ? "bg-blue-500 text-white lg:bg-transparent lg:text-blue-500" : "hover:bg-gray-100"}`}
+                className={`lg:hover:bg-transparent lg:hover:text-blue-500 py-2 px-3 cursor-pointer rounded ${index === 0 ? "bg-blue-500 text-white lg:bg-transparent lg:text-blue-500" : "hover:bg-gray-100"} ${(index === 3 || index === 4) && "lg:text-white"}`}
                 key={`${index}${route}`}
               >
                 {route}
@@ -32,8 +37,8 @@ const Nav = () => {
       </div>
 
       {/* Cart button */}
-      <div className="fixed bottom-4 left-4 lg:static">
-        <div className="flex-center h-12 w-12 rounded-full bg-white shadow-md">
+      <div className="fixed bottom-4 left-4 lg:static lg:mr-8">
+        <div className="flex-center cursor-pointer h-12 w-12 rounded-full bg-white shadow-md">
           <TbShoppingBag />
         </div>
       </div>
